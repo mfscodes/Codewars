cfca6195970d29131f9cfcd27543bd0833cc9d87
mfscodes <mfscodes@gmail.com>
2022-08-05 20:08:42 -0500

    adding solution

--- js/solution032.js
+++ js/solution032.js
@@ -1,1 +1,4 @@
 //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+function sumMix(x) {
+    return x.reduce((s, n) => s + Number(n), 0)
+  }
