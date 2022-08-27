63d799184a55858a267886ca12994cc34b3a6b41
Mary <mfscodes@gmail.com>
2022-08-27 14:34:12 -0500

    adding codewars

---
+++ js/solution041.js
@@ -0,0 +1,11 @@
+
+// My PC got infected by a strange virus. It only infects my text files and replaces random letters by *, li*e th*s (like this).
+
+// Fortunately, I discovered that the virus hides my censored letters inside root directory.
+
+// It will be very tedious to recover all these files manually, so your goal is to implement uncensor function that does the hard work automatically
+
+function uncensor(infected, discovered) {
+    const arr = [...discovered]
+    return [...infected].map(el=> el === '*' ? arr.shift() : el).join('')
+  }
