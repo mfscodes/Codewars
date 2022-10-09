bd64f088df452808d7929ad99638c24d24616eee
mfscodes <mfscodes@gmail.com>
2022-10-09 18:54:58 -0500

    adding codewars solution

--- js/solution067.js
+++ js/solution067.js
@@ -6,3 +6,6 @@
 // If a value is present in b, all of its occurrences must be removed from the other:
 
 // arrayDiff([1,2,2,2,3],[2]) == [1,3]
+function array_diff(a, b) {
+    return a.filter(e => !b.includes(e));
+  }
